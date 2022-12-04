6d555642399db9da3b9b228245da2a374633a895
Emin <eminyilmzz@gmail.com>
2022-12-05 00:00:19 +0300

    Middle of the Linked List -> https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ src/Solutions/MiddleoftheLinkedList/Solution.java
@@ -0,0 +1,20 @@
+package Solutions.MiddleoftheLinkedList;
+
+public class Solution {
+    public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
