d3da81a8e71b02c5f28241e5adf8520e46b876b6
Emin <eminyilmzz@gmail.com>
2024-03-23 22:42:10 +0300

    (Go) 49. Group Anagrams -> https://leetcode.com/problems/group-anagrams/

--- src/Solutions/GroupAnagrams/solution.go
+++ src/Solutions/GroupAnagrams/solution.go
@@ -1,19 +1,19 @@
 package GroupAnagrams
 
 func groupAnagrams(strs []string) [][]string {
-    anagramMap := make(map[[26]int][]string)
-    for _, s := range strs {
-        var count [26]int
-        for _, c := range s {
-            count[c - 'a']++
-        }
-        anagramMap[count] = append(anagramMap[count], s)
-    }
-    result := make([][]string, len(anagramMap))
-    idx := 0
-    for _, v := range anagramMap {
-        result[idx] = v
-        idx++
-    }
-    return result
+	grouped := make(map[[26]int][]string)
+	for _, s := range strs {
+		var count [26]int
+		for _, c := range s {
+			count[c-'a']++
+		}
+		grouped[count] = append(grouped[count], s)
+	}
+	result := make([][]string, len(grouped))
+	idx := 0
+	for _, v := range grouped {
+		result[idx] = v
+		idx++
+	}
+	return result
 }
